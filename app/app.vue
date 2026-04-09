f5ac89c3b4042fd5ae6f5a67d9e227a201fbd164
huff-laptop <benni.huff@gmail.com>
2026-04-09 12:39:42 +0200

    stuff

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,19 @@
   <div>
     <h1>works</h1>
     <button @click="subscribe()">trigger</button>
+
+    <button @click="funct()">sent</button>
+
   </div>
 </template>
 
 <script setup lang="ts">
+async function funct() {
+  await $fetch('/api/send-push', {
+    method: 'POST',
+  });
+}
+
 async function subscribe() {
   const permission = await Notification.requestPermission();
   if (permission !== 'granted') return;
